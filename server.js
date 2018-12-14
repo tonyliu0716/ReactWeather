c4dcbe4e2d2e371aca05e8a76110550fa73324f9
tonyliu0716 <tonyliu0716@gmail.com>
2018-12-13 20:00:53 -0600

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ var express = require('express');
 // create our app
 var app = express();
 
+// heroku needs to find a different port to deploy
+const PORT = process.env.PORT || 3000;
+
+// transfer HTTP to HTTPS
+app.use(function (req, res, next) {
+    if (res.headers['x-forwarded-proto'] === 'http') {
+        // process the next request
+        next();
+    } else {
+        // send https to http
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('Express server is up on 3000.');
+app.listen(PORT, function () {
+    console.log('Express server is up on port ' + PORT);
 });
