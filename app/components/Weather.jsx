18d20cf49e4023e7fbe6e915fe49641ab9ce0593
tonyliu0716 <tonyliu0716@gmail.com>
2018-12-18 22:13:25 -0600

    udpate error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -50,7 +50,7 @@ var Weather = React.createClass({
         function renderError() {
             if(typeof errorMessage === 'string') {
                 return (
-                    <ErrorModal />
+                    <ErrorModal message={errorMessage}/>
                 );
             }
         }
